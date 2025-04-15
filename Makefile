ab436f813b8a89becb943668c530a33010a19348
ErikG <eriklustosa@gmail.com>
2025-04-15 13:38:37 +0100

    idk

--- Makefile
+++ Makefile
@@ -1,9 +1,25 @@
-setup:
-	cd srcs/requirements/nginx/tools && ./generate_ssl.sh
-build:
-	cd srcs && docker-compose build
+DOCKER_COMPOSE = srcs/docker-compose.yml
+
+.PHONY: all up down clean fclean re
+
+all: up
+
 up:
-	cd srcs && docker-compose up
+	mkdir -p data/wp
+	mkdir -p data/db
+	docker-compose -f $(DOCKER_COMPOSE) up -d --build
+
+
+
 down:
-	cd srcs && docker-compose down
-restart: down build up
+	docker-compose -f $(DOCKER_COMPOSE) down
+
+clean: down
+	docker system prune -a
+
+fclean: clean
+	docker volume rm $$(docker volume ls -q)
+	sudo rm -rf data/wp/*
+	sudo rm -rf data/db/*
+
+re: fclean all
---
+++ srcs/requirements/mariadb/tools/mariadb_setup.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
+chown -R mysql:mysql /var/lib/mysql
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	mysql_install_db --user=mysql --datadir=/var/lib/mysql
+	/usr/bin/mysqld --user=mysql --bootstrap << EOF
+USE mysql;
+FLUSH PRIVILEGES;
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+fi
+
+exec /usr/bin/mysqld --user=mysql --console
