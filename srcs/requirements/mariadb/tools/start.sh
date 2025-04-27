fc9037ab04b1ae2a9ecb925fe26ec784a168ab4b
ErikG <eriklustosa@gmail.com>
2025-04-27 02:31:59 +0100

    foi?

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
 set -e
 
-DB_ROOT_PASS=$(</run/secrets/dbrootpass)
+DB_ROOT_PASS=$(cat /run/secrets/dbrootpass)
+WP_DB_PASS=$(cat /run/secrets/dbpass)
+
+echo root pass: $DB_ROOT_PASS db pass: $WP_DB_PASS
 
 chown -R mysql:mysql /run/mysqld /var/lib/mysql
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-	echo "→  Initialising fresh MariaDB data dir"
+	echo "Initialising fresh MariaDB"
+
 	su-exec mysql mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
+	su-exec mysql mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
+	pid="$!"
+	sleep 5
+
+	mysql -u root <<-EOSQL
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';
+		CREATE DATABASE IF NOT EXISTS \`$WP_DB_NAME\`;
+		CREATE USER IF NOT EXISTS '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_DB_PASS';
+		GRANT ALL PRIVILEGES ON \`$WP_DB_NAME\`.* TO '$WP_DB_USER'@'%';
+		FLUSH PRIVILEGES;
+EOSQL
+
+	kill "$pid"
+	wait "$pid"
 fi
 
-exec su-exec mysql mysqld "$@"
+echo "bind-address=0.0.0.0" > /etc/my.cnf.d/mariadb-server.cnf
+
+exec su-exec mysql mysqld
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -4,33 +4,34 @@ set -e
 WP_ADMIN_PASS=$(</run/secrets/wpadmin)
 WP_DB_PASS=$(</run/secrets/dbpass)
 
-#echo "Waiting for MariaDB to be ready..."
-#until mysqladmin ping -h"$WP_DB_HOST" --silent; do
-#	sleep 1
-#done
-#echo "MariaDB is ready."
+echo "Waiting for MariaDB to be ready..."
+until mysqladmin ping -h"$WP_DB_HOST" -u"$WP_DB_USER" -p"$WP_DB_PASS" --silent; do
+	sleep 2
+done
+echo "MariaDB is ready!"
 
-if [ -f ./wp-config.php ]; then
-then
-	echo "Wordpress already Downloaded!"
-else
-	echo "Downloading WordPress..."
+if [ ! -f wp-config.php ]; then
+	echo "Downloading & Installing WordPress..."
+
 	wp core download --allow-root
 
-	echo "Creating wp-config.php..."
 	wp config create --allow-root \
-		--dbname="$WP_DB_NAME" --dbuser="$WP_DB_USER" \
-		--dbpass="$WP_DB_PASS" --dbhost="$WP_DB_HOST" \
+		--dbname="$WP_DB_NAME" \
+		--dbuser="$WP_DB_USER" \
+		--dbpass="$WP_DB_PASS" \
+		--dbhost="$WP_DB_HOST" \
 		--skip-check
 
-	echo "Installing WordPress..."
 	wp core install --allow-root \
 		--url="https://$DOMAIN_NAME" \
-		--title="$WP_TITLE" --admin-user="$WP_ADMIN_USER" \
+		--title="$WP_TITLE" \
+		--admin_user="$WP_ADMIN_USER" \
 		--admin_password="$WP_ADMIN_PASS" \
 		--admin_email="$WP_ADMIN_EMAIL"
+
+	echo "WordPress installed!"
+else
+	echo "WordPress is already installed."
 fi
 
-chown -R www-data:www-data /var/www/html
-
-exec php-fpm82 --nodaemonize
+exec php-fpm83 -F
